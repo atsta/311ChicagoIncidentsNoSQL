cf9b86fc5de9f31003c01ca5b9fa50ff98996f76
atsta <atstamatiou@gmail.com>
2021-01-07 00:24:55 +0200

    incidents insertion

--- data_insert.py
+++ data_insert.py
@@ -1,27 +1,58 @@
 import csv
 import json
+import datetime
 import pandas as pd
 import sys, getopt, pprint
 from pymongo import MongoClient
 
-datapath = '/home/atstam/Documents/Master CS/Database Systems/Project 2/Data/'
-csv_title = '311-service-requests-alley-lights-out'
-
-csvfile = open(datapath + csv_title + '.csv', 'r')
-reader = csv.DictReader(csvfile)
-
 mongo_client = MongoClient('localhost', 27017)
 db = mongo_client.NoSQL_311_Chicago_Incidents
 
-alley_lights_headers = ["Creation Date", "Status", "Completion Date", "Service Request Number", "Type of Service Request", "Street Address", "ZIP Code", "X Coordinate",	"Y Coordinate", "Ward",	"Police District",	"Community Area",	"Latitude",	"Longitude",	"Location",	"Historical Wards 2003-2015",	"Zip Codes",	"Community Areas",	"Census Tracts",	"Wards"]
+csv_file_titles = ["311-service-requests-abandoned-vehicles",
+                    "311-service-requests-alley-lights-out",
+                    "311-service-requests-garbage-carts",
+                    "311-service-requests-graffiti-removal",
+                    "311-service-requests-pot-holes-reported",
+                    "311-service-requests-rodent-baiting",
+                    "311-service-requests-sanitation-code-complaints",
+                    "311-service-requests-street-lights-all-out",
+                    "311-service-requests-street-lights-one-out",
+                    "311-service-requests-tree-debris",
+                    "311-service-requests-tree-trims"]
 
-#alley_lights_fields = [creation_date,status,completion_date,service_request_number,type_of_service_request, street_address,zip_code,x_coordinate,y_coordinate,ward,police_district,community_area, latitude,longitude,location,historical_wards,zip_codes,community_areas,census_tracts,wards
-'''
-for each in reader:
-    row={}
-    for field in alley_lights_headers :
-        row[field]=each[field]
-    db.incident.insert_one(row)
-'''
-#delete all documents of incidents collection
-db.incident.delete_many({ })
+def data_clean(row, fields, incident_type):
+    if incident_type == "311-service-requests-pot-holes-reported":
+        row['Type of Service Request'] = "Pothole in Street"
+    if incident_type == "311-service-requests-street-lights-one-out":
+        row['Type of Service Request'] = "Street Light Out"
+
+    del row['Location']
+
+    if incident_type == "311-service-requests-rodent-baiting":
+        del row['']
+
+    row['Creation Date'] = datetime.datetime.strptime(row['Creation Date'], '%Y-%m-%dT%H:%M:%S.%f')
+    if row['Completion Date'] != "":
+        row['Completion Date'] = datetime.datetime.strptime(row['Completion Date'], '%Y-%m-%dT%H:%M:%S.%f')
+
+    return row
+
+datapath = '/home/atstam/Documents/Master CS/Database Systems/Project 2/Data/'
+
+for title in csv_file_titles:
+    count = 0
+
+    csvfile = open(datapath + title + '.csv', 'r')
+    reader = csv.DictReader(csvfile)
+
+    headers = reader.fieldnames
+
+    for row in reader:
+        #print(row)
+        if title == "311-service-requests-rodent-baiting":
+            if row[''] != "":
+                continue
+        db.incident.insert_one(data_clean(row, headers, title))
+        count = count + 1
+
+    print("Inserted " + str(count) + " incidents from " + title + " CSV file")
