2df7eb869251d5a3dd53c39606c5537a9d8b0b6f
atsta <atstamatiou@gmail.com>
2021-01-10 01:32:58 +0200

    add query 1

--- app.py
+++ app.py
@@ -8,9 +8,14 @@ app = Flask(__name__)
 mongo_client = MongoClient('localhost', 27017)
 db = mongo_client.NoSQL_311_Chicago_Incidents
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/query1', methods = ['GET'])
+def _query1():
+    _json = request.json
+    _start_date = _json['Start Date']
+    _end_date = _json['End Date']
+
+    res = queries.query1(_start_date, _end_date)
+    return res
 
 @app.route('/query2', methods = ['GET'])
 def _query2():
@@ -19,9 +24,7 @@ def _query2():
     _end_date = _json['End Date']
     _type = _json['Type']
 
-    print(_start_date, _end_date, _type)
     res = queries.query2(_start_date, _end_date, _type)
-
     return res
 
 
--- queries.py
+++ queries.py
@@ -7,6 +7,18 @@ import json
 mongo_client = MongoClient('localhost', 27017)
 db = mongo_client.NoSQL_311_Chicago_Incidents
 
+def query1(start_date, end_date):
+    res = db.incident.aggregate([
+        {"$match": {"$expr": {"$and": [{"$gte": ["$creation_date", {"$dateFromString": {"dateString": start_date}}]},
+                                       {"$lte": ["$creation_date", {"$dateFromString": {"dateString": end_date}}]}]}}},
+        {"$group": {"_id": {"type":"$type", "creation_date": "$creation_date"}, "count": {"$sum": 1}}},
+
+        {"$project": {"_id": 0, "creation_date": {"$dateToString": {"date": "$_id.creation_date"}}, "type": "$_id.type","count": "$count"}},
+        {"$sort": {"count": -1}}
+    ])
+
+    return json.dumps(list(res))
+
 def query2(start_date, end_date, type):
     res = db.incident.aggregate([
         {"$match": {"type": type}},
